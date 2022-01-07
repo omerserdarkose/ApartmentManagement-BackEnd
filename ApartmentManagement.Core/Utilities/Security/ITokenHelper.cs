9d7897fda2dc9eebace246ad49113a0caa733f56
omerserdarkose <omerserdarkose@gmail.com>
2022-01-07 05:20:42 +0300

    Add JWT
    A helper has been written for securityKey and signingcredentials. Extension methods have been added to add claims in jwt configuration

---
+++ ApartmentManagement.Core/Utilities/Security/ITokenHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApartmentManagement.Core.Entities.Concrete;
+
+namespace ApartmentManagement.Core.Utilities.Security
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user,List<Claim> userClaims);
+    }
+}
---
+++ ApartmentManagement.Core/Utilities/Security/JWT/JWTHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApartmentManagement.Core.Entities.Concrete;
+using ApartmentManagement.Core.Extensions;
+using ApartmentManagement.Core.Utilities.Security.Encryption;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace ApartmentManagement.Core.Utilities.Security.JWT
+{
+    public class JWTHelper:ITokenHelper
+    {
+        private IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
+        private DateTime _accessTokenExpiration;
+
+        public JWTHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
+        }
+
+        public AccessToken CreateToken(User user, List<Claim> userClaims)
+        {
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+            var jwt = CreatJwtSecurityToken(_tokenOptions, user, signingCredentials, userClaims);
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var token = jwtSecurityTokenHandler.WriteToken(jwt);
+
+            return new AccessToken()
+            {
+                Token = token,
+                Expiration = _accessTokenExpiration
+            };
+
+        }
+
+        private JwtSecurityToken CreatJwtSecurityToken(TokenOptions tokenOptions, User user,
+            SigningCredentials signingCredentials, List<Claim> userClaims)
+        {
+            var jwt = new JwtSecurityToken(
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                expires: _accessTokenExpiration,
+                notBefore: DateTime.Now,
+                signingCredentials: signingCredentials,
+                claims: SetClaims(user, userClaims)
+            );
+
+            return jwt;
+        }
+
+        private IEnumerable<System.Security.Claims.Claim> SetClaims(User user, List<Claim> userClaims)
+        {
+            var claims = new List<System.Security.Claims.Claim>();
+
+            claims.AddNameIdentifier(user.Id.ToString());
+            claims.AddEmail(user.Email);
+            claims.AddName($"{user.FirstName} {user.LastName}");
+            claims.AddRoles(userClaims.Select(x=>x.Name).ToArray());
+
+            return claims;
+        }
+    }
+}
