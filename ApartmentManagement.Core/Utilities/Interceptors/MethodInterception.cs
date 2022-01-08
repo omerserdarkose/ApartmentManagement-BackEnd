00bbb1f3384b65ee66cff7f18f671519c3f51d09
omerserdarkose <omerserdarkose@gmail.com>
2022-01-08 07:21:41 +0300

    Add fundamentals of Aspect structure

---
+++ ApartmentManagement.Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Core.Interceptor;
+
+namespace ApartmentManagement.Core.Utilities.Interceptors
+{
+    public abstract class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        /// <summary>
+        /// kullanacak aspectler icin temel akisi iceren ve override edilerek ihtiyaca yonelik
+        /// doldurulacak methodlari iceren soyut sinif
+        /// </summary>
+
+        //IInvocation calisacak olan methodu temsil ediyor
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation) { }
+        protected virtual void OnSuccess(IInvocation invocation) { }
+
+        //genel akis
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception)
+            {
+                isSuccess = false;
+                OnException(invocation);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}
---
+++ ApartmentManagement.Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Core.Interceptor;
+
+namespace ApartmentManagement.Core.Utilities.Interceptors
+{
+    /// <summary>
+    /// kullanacagimiz butun method interceptorlari icin genel ozellikleri iceren bir baseAttribute sinifi olusturuyoruz
+    /// </summary>
+
+    //bu attribute methodlara uygulanabilir,birden fazla kez kullanilabilir
+    [AttributeUsage(AttributeTargets.Method,AllowMultiple = true)]
+    public abstract class MethodInterceptionBaseAttribute:Attribute,IInterceptor
+    {
+        public int Priority { get; set; }
+        public virtual void Intercept(IInvocation invocation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
